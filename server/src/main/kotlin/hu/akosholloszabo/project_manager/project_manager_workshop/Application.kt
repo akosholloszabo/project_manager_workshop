5a53f7fa76f9b1a5647da8770b462d6962b54efb
holloszaboakos <holloszaboakos.1996@gmail.com>
2026-01-11 19:49:19 +0100

    removed koin from server too

--- server/src/main/kotlin/hu/akosholloszabo/project_manager/project_manager_workshop/Application.kt
+++ server/src/main/kotlin/hu/akosholloszabo/project_manager/project_manager_workshop/Application.kt
@@ -5,6 +5,12 @@ import hu.akosholloszabo.project_manager.project_manager_workshop.controller.not
 import hu.akosholloszabo.project_manager.project_manager_workshop.controller.projectRoutes
 import hu.akosholloszabo.project_manager.project_manager_workshop.controller.ticketRoutes
 import hu.akosholloszabo.project_manager.project_manager_workshop.db.DatabaseFactory
+import hu.akosholloszabo.project_manager.project_manager_workshop.repository.NoteRepository
+import hu.akosholloszabo.project_manager.project_manager_workshop.repository.ProjectRepository
+import hu.akosholloszabo.project_manager.project_manager_workshop.repository.TicketRepository
+import hu.akosholloszabo.project_manager.project_manager_workshop.service.NoteService
+import hu.akosholloszabo.project_manager.project_manager_workshop.service.ProjectService
+import hu.akosholloszabo.project_manager.project_manager_workshop.service.TicketService
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.config.*
@@ -13,8 +19,6 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
-import org.koin.ktor.ext.get
-import org.koin.ktor.plugin.Koin
 
 fun main() {
     val runtimeConfig = HoconApplicationConfig(ConfigFactory.load())
@@ -35,12 +39,17 @@ fun Application.module(config: ApplicationConfig = environment.config) {
     }
     install(CallLogging)
     DatabaseFactory.init(config)
-    install(Koin) {
-        modules(serverModule)
-    }
+
+    val projectRepository = ProjectRepository()
+    val projectService = ProjectService(projectRepository)
+    val noteRepository = NoteRepository()
+    val noteService = NoteService(noteRepository)
+    val ticketRepository = TicketRepository()
+    val ticketService = TicketService(ticketRepository)
+
     routing {
-        projectRoutes(get())
-        noteRoutes(get())
-        ticketRoutes(get())
+        projectRoutes(projectService)
+        noteRoutes(noteService)
+        ticketRoutes(ticketService)
     }
 }
--- server/src/main/kotlin/hu/akosholloszabo/project_manager/project_manager_workshop/ServerModule.kt
+++ server/src/main/kotlin/hu/akosholloszabo/project_manager/project_manager_workshop/ServerModule.kt
@@ -1,19 +1,4 @@
 package hu.akosholloszabo.project_manager.project_manager_workshop
 
-import hu.akosholloszabo.project_manager.project_manager_workshop.repository.NoteRepository
-import hu.akosholloszabo.project_manager.project_manager_workshop.repository.ProjectRepository
-import hu.akosholloszabo.project_manager.project_manager_workshop.repository.TicketRepository
-import hu.akosholloszabo.project_manager.project_manager_workshop.service.NoteService
-import hu.akosholloszabo.project_manager.project_manager_workshop.service.ProjectService
-import hu.akosholloszabo.project_manager.project_manager_workshop.service.TicketService
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.module
-
-val serverModule = module {
-    singleOf(::ProjectRepository)
-    singleOf(::ProjectService)
-    singleOf(::NoteRepository)
-    singleOf(::NoteService)
-    singleOf(::TicketRepository)
-    singleOf(::TicketService)
-}
+@Deprecated("Koin removed; services are wired manually in Application.module")
+object ServerModule
